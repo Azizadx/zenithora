a212ed35aea003ce5dad42837b1204a88a2daf95
azizadx <azizadx.tech@gmail.com>
2023-08-31 17:42:53 +0300

    lib/config the bracket-lin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Welcome to Zenithora");
+use bracket_lib::prelude::*;
+struct State {}
+impl GameState for State {
+   fn tick(&mut self, ctx: &mut BTerm) {
+       ctx.cls();
+        ctx.print(1, 1, "Welcome to Zenithora")
+   } 
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+                .with_title("Zenithora")
+                .build()?;
+    main_loop(context, State{})
 }
