f6ab0fdef98cb644b753fc63f1622e25ac40c3d7
azizadx <azizadx.tech@gmail.com>
2023-09-01 15:38:59 +0300

    feat/add-player-to-map

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 //Import Dir
 mod  map;
+mod player;
 mod prelude {
    pub use bracket_lib::prelude::*;
    pub const SCREEN_WIDTH: i32 = 80; 
    pub const SCREEN_HEIGHT: i32 = 50;
    pub use crate::map::*;
+   pub use crate::player::*;
 }
 //Import Lib
 use prelude::*;
@@ -12,11 +14,15 @@ use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map:Map::new() }
+        Self { 
+            map:Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        }
     }
     
 }
@@ -24,7 +30,9 @@ impl State {
 impl GameState for State {
    fn tick(&mut self, ctx: &mut BTerm) {
        ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
    } 
 }
 fn main() -> BError {
--- src/map.rs
+++ src/map.rs
@@ -18,11 +18,28 @@ pub struct Map {
 pub fn map_index(x:i32, y:i32) -> usize {
    ((y*SCREEN_WIDTH) + x) as usize
 }
+
+
 impl Map {
     pub fn new() -> Self {
         Self { tiles: vec![TileType::Floor;NUM_TILES] }
     }
-
+    //
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >=0 && point.y < SCREEN_HEIGHT
+    }
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point)
+            && self.tiles[map_index(point.x, point.y)] == TileType::Floor
+    }
+    pub fn try_index(&self, point:Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        }
+        else {
+            Some(map_index(point.x, point.y))
+        }
+    }
    
     pub fn render(&self, ctx: &mut BTerm) {
         for y in 0..SCREEN_HEIGHT {
@@ -39,6 +56,7 @@ impl Map {
             }
         }
     }
+   
     
 }
 
