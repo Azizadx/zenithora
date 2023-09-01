df6b1096765e64349a6d8763664051a0b877d24c
azizadx <azizadx.tech@gmail.com>
2023-09-01 20:38:02 +0300

    feat/setup map builder for wall

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 //Import Dir
-mod  map;
+mod map;
+mod map_builder;
 mod player;
 mod prelude {
-   pub use bracket_lib::prelude::*;
-   pub const SCREEN_WIDTH: i32 = 80; 
-   pub const SCREEN_HEIGHT: i32 = 50;
-   pub use crate::map::*;
-   pub use crate::player::*;
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 //Import Lib
 use prelude::*;
-
 
 struct State {
     map: Map,
@@ -19,26 +20,27 @@ struct State {
 
 impl State {
     fn new() -> Self {
-        Self { 
-            map:Map::new(),
-            player: Player::new(Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        Self {
+            map: Map::new(),
+            player: Player::new(map_builder.player_start),
         }
     }
-    
 }
 
 impl GameState for State {
-   fn tick(&mut self, ctx: &mut BTerm) {
-       ctx.cls();
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
         self.player.update(ctx, &self.map);
         self.map.render(ctx);
         self.player.render(ctx);
-   } 
+    }
 }
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-                .with_title("Zenithora")
-                .with_fps_cap(30.0)
-                .build()?;
+        .with_title("Zenithora")
+        .with_fps_cap(30.0)
+        .build()?;
     main_loop(context, State::new())
 }
